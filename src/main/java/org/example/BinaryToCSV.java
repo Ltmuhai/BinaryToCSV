668955e0f428f9708a5025f178e09ca367bf29f5
Ltmuhai <ltmuhai@gmail.com>
2024-08-11 22:26:55 +0800

    first 100 data

--- src/main/java/org/example/BinaryToCSV.java
+++ src/main/java/org/example/BinaryToCSV.java
@@ -1,15 +1,46 @@
 package org.example;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.*;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 public class BinaryToCSV {
+
+    public static CSVFormat customCsvFormat(String[] CSV_HEAD){
+        return CSVFormat.Builder.create()
+                .setAllowMissingColumnNames(false) // 不允许丢失字段名称，默认为True
+                .setDelimiter(",") //默认为逗号
+                .setHeader(CSV_HEAD) // CSV 表头
+                .setTrim(true) // 去除数据两边的空格，如 "Abc " 则实际输出为"Abc"，但是数据为"A bc",实际输出还是"A bc"
+                .build();
+    }
+    public static void generateCsvWithConfig(List<String[]> Strs, CSVFormat csvFormat){
+        // 可以通过设置FileWriter的编码来控制输出文件的编码格式
+        // FileWriter fileWriter = new FileWriter("ApacheCsv.csv", StandardCharsets.UTF_8);
+        try(FileWriter fileWriter = new FileWriter("0001.csv", StandardCharsets.UTF_8);
+            CSVPrinter csvPrinter = new CSVPrinter(fileWriter, csvFormat)){
+            // 会将整个String 列表作为一条数据行写入
+            // csvPrinter.printRecord(users);
+            // 默认配置不会写表头，如果需要添加表头可以单独设置表头
+            for (String[] str : Strs) {
+                csvPrinter.printRecord(str);
+            }
+            // 输出一个空行
+            csvPrinter.println();
+            csvPrinter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public static void main(String[] args) {
         String binaryFilePath = "src/样本.txt";//二级制文件地址
         //String csvFilePath = "path/to/your/outputfile.csv";//csv文件地址
-        //String excelFilePath = "path/to/your/outputfile.csv";//excel文件地址
+        String[] headerArr = new String[]{"名字", "公司", "介绍","公司", "地址", "身份证", "时间", "法人", "注册金额", "编号", "网址"};
         char a = 1;
         try {
             //File file = new File(binaryFilePath);
@@ -19,23 +50,23 @@ public class BinaryToCSV {
             //BufferedInputStream bis = new BufferedInputStream(fis);
             String line ;
             int n = 0;
+            List<String[]> row = new ArrayList<>();
             while ((line = fis.readLine()) != null) {
                 // 处理每一行的逻辑
                 String[] values = line.split("\u0001"); // 使用0001分割每一行的值
-                List<String> row = new ArrayList<>();
-                for (String value : values) {
-                    System.out.println(value);
-                    row.add(value.trim()); // 去除每个值的空格并添加到行中
-                }
+                System.out.println(Arrays.toString(values));
+                row.add(values);
                 n++;
                 if (n>=100){
                     break;
                 }
             }
+            generateCsvWithConfig(row, customCsvFormat(headerArr));
             fis.close();
         } catch (IOException ex) {
             System.out.println(ex);
         }
+
     }
 }
 
